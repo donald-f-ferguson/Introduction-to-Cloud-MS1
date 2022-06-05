9742e1c82336f42e538fc350ff0704c6216cb6d1
donaldferguson <donff2@aol.com>
2022-06-05 09:00:07 -0400

    Added missing files.

--- src/resources/rdb_service_base.py
+++ src/resources/rdb_service_base.py
@@ -23,3 +23,43 @@ class RDBServiceBase(DataServiceBase):
 
     def get_by_id(self, id):
         raise NotImplemented()
+
+    def _get_where_clause_args(self, template):
+
+        w_clause = ""
+        args = None
+
+        if template:
+
+            terms = []
+            args = []
+
+            for k,v in template.items():
+
+                terms.append(k + "=%s")
+                args.append(v)
+
+            w_clause = " where " + " and ".join(terms)
+
+        return w_clause, args
+
+    def _run_q(self, sql, args):
+
+        conn = self.get_connection()
+        cursor = conn.cursor()
+
+        print("sql = ", cursor.mogrify(sql, args))
+
+    def find_by_template(self, collection, template=None, fields=None, limit=None, offset=None, order_by=None):
+
+        sql = "select {field_list} from " + collection
+
+        if fields:
+            field_list = ",".join(fields)
+        else:
+            fields = "*"
+
+        sql1 = sql.format(field_list=field_list)
+        wc = self._get_where_clause_args(template)
+        sql2 = sql1 + wc
+
