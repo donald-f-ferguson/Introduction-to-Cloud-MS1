1ef671fd251e60b938c3c41156a93662aa94b46b
donaldferguson <donff2@aol.com>
2022-06-05 09:11:00 -0400

    Added missing files.

---
+++ src/app_env.sh
@@ -0,0 +1,2 @@
+export DB_CONNECT_INFO='{"user": "dbuser", "password": "AnsysIntr02Cloud!X7", "host": "localhost", "db": "intro_cloud"}'
+export PYTHONPATH=$PYTHONPATH:/home/ubuntu/Introduction-to-Cloud-MS1
